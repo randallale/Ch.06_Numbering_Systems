b0b239413cdfd93d4a597476ea057d00d0fc8969
Alex <randallale@urbandaleschools.com>
2019-02-04 22:20:02 -0600

    Finished

--- 6.0_Take_Home_Test.py
+++ 6.0_Take_Home_Test.py
@@ -1,22 +1,22 @@
 '''
 HONOR CODE: I solemnly promise that while taking this test I will only use any resources except my brain.
-Signed: ______________________
+Signed: Alexander Randall
 
 
 Convert Binary 101010 to Hexadecimal.
-
+2A
 Convert Binary 11101001 to Octal.
-
+351
 Convert Hexadecimal FC to Binary.
-
+11111100
 Convert Hexadecimal 1F to Decimal.
-
+31
 Convert Hexadecimal #FAAFBD to RGB.
-
+(250,175,189) A more reddish color
 Convert Octal 70 to Binary.
-
+111000
 Convert RGB (32,128,64) to Hexadecimal.
-
+208040
 Convert 01000001, 01010011, 01000011, 01001001, 01001001 to ASCII.
-
+ASCII very funny
 '''
